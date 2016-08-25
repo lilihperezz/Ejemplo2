56517faedd6f388c48126c65bfedca8793c06781
lili hualtibamba <lilihperezz@gmail.com>
2016-08-25 15:34:48 -0500

    agregando files promedio

---
+++ js/app.js
@@ -0,0 +1,9 @@
+
+
+var numero1= prompt("Ingrese primer número");
+var numero2 = prompt("Ingrese segundo número");
+var numero3 = prompt("Ingrese tercer número");
+
+	var resultado = ((parseInt(numero1)+parseInt(numero2)+parseInt(numero3))/3);
+	document.write('<br>');
+	document.write("El resultado del promedio es " + resultado);
